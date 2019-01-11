cf6b82e39d286ec3945f571b6e4732cc7b8fbb59
tigerism <22227890+tigerism@users.noreply.github.com>
2019-01-10 23:40:27 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(name=“getclass.py”,
+	version=“0.0.1”,
+	url="https://github.com/looking-for-a-job/getclass.py",
+	packages=find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3.5",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"Operating System :: OS Independent"
+	],
+	zip_safe=False
+)
